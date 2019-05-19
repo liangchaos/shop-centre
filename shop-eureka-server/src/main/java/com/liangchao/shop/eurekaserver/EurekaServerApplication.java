7d0cee1cc94db149d4af9bf8ccd3e8b3b463c109
lianchaos <245073271@qq.com>
2019-05-19 21:45:22 +0800

    创建

--- shop-eureka-server/src/main/java/com/liangchao/shop/eurekaserver/EurekaServerApplication.java
+++ shop-eureka-server/src/main/java/com/liangchao/shop/eurekaserver/EurekaServerApplication.java
@@ -1,14 +1,22 @@
 package com.liangchao.shop.eurekaserver;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * Description:服务治理
  * User: Administrator-LiangChao
  * Date: 2019-05-19
  * Time: 15:53
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
-    public static void main(String[] args){
-
+    public static void main (String[] args) {
+        SpringApplicationBuilder springApp = new SpringApplicationBuilder (EurekaServerApplication.class);
+        springApp.web (WebApplicationType.SERVLET);
+        springApp.run (args);
     }
 }

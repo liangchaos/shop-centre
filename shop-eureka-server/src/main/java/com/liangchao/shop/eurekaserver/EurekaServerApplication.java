b1454ffed4ceb6d2bcb99cfed35a60000416b023
lianchaos <245073271@qq.com>
2019-05-19 16:04:10 +0800

    创建

---
+++ shop-eureka-server/src/main/java/com/liangchao/shop/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.liangchao.shop.eurekaserver;
+
+/**
+ * Description:服务治理
+ * User: Administrator-LiangChao
+ * Date: 2019-05-19
+ * Time: 15:53
+ */
+public class EurekaServerApplication {
+
+    public static void main(String[] args){
+
+    }
+}
